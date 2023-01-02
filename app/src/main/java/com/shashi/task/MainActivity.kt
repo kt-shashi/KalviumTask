ffe2713f033599bc18c13469a7f8cf5a0ca724c9
shashi-kant10 <shashiofficial10@gmail.com>
2023-01-02 21:35:31 +0530

    Added crop and display feature

--- app/src/main/java/com/shashi/task/MainActivity.kt
+++ app/src/main/java/com/shashi/task/MainActivity.kt
@@ -1,12 +1,56 @@
 package com.shashi.task
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import com.github.dhaval2404.imagepicker.ImagePicker
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var image: ImageView
+    private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        button = findViewById(R.id.btnImage)
+        image = findViewById(R.id.ivImage)
+
+        button.setOnClickListener {
+            chooseImage()
+        }
+
     }
+
+    fun chooseImage() {
+        ImagePicker.with(this)
+            .crop()
+            .compress(512)
+            .galleryOnly()
+            .maxResultSize(
+                1080,
+                1080
+            )
+            .start()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            val uri: Uri = data?.data!!
+
+            image.setImageURI(uri)
+        } else if (resultCode == ImagePicker.RESULT_ERROR) {
+            Toast.makeText(this, ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Task Cancelled", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
